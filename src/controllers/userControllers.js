36ee5c636bb054b883b6b91ede498152e30fd89d
GuilhermeFreitass <guilhermeaqf@gmail.com>
2024-02-17 21:21:45 -0300

    Adicionei as rotas de PUT e DELETE

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,4 @@
-const users = require('../mocks/users');
+let users = require('../mocks/users');
 
 module.exports = {
     listUsers(request, response){
@@ -33,11 +33,56 @@ module.exports = {
         const lastUserId = users[users.length -1].id;
         const newUser = {
             id: lastUserId + 1,
+            cpf: body.cpf,
             nome: body.nome,
+            data_Nascimento: body.data_Nascimento
         };
 
         users.push(newUser)
         
         response.send(200, newUser);
         },
+
+    
+    updateUser(request, response){
+        let {id} = request.params;
+        const {
+         cpf,
+         nome,
+         data_Nascimento} = request.body;
+        
+
+        id = Number(id);
+
+        const userExists = users.find((user) => user.id == id);
+
+        if(!userExists){
+            return response.send(400, {error: 'User not found' })
+        }
+
+        users = users.map((user) => {
+            if(user.id == id){
+                return{
+                    ...user,
+                    cpf,
+                    nome,
+                    data_Nascimento
+                };
+            }
+            return user
+        });
+
+        response.send(200, {id, cpf, nome, data_Nascimento });
+
+    },
+    
+    deleteUser(request,response){
+        let {id} = request.params;
+        id = Number(id);
+
+        users = users.filter((user) => user.id != id);
+
+        response.send(200, {deleted: true});
+    }
+
     };
